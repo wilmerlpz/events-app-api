00da5c846563f90a141f4602c8a3c483dbdc3e5f
Wilmer López <wilmerlopez08@gmail.com>
2018-10-30 17:58:59 -0600

    First Commit

---
+++ create.js
@@ -0,0 +1,35 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "events",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      eventId: uuid.v1(),
+      name: data.name,
+      description: data.description,
+      location:data.location,
+      latitude:data.latitude, 
+      longitude:data.longitude,
+      status: data.status,
+      category:data.category,
+      country: data.country,
+      state:data.state,
+      city:data.city,
+      mainPhoto: data.mainPhoto,
+      createdAt: Date.now(),
+      modifiedAt: Date.now(),
+      updatedAt: Date.now(),
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
---
+++ update.js
@@ -0,0 +1,32 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "notes",
+    // 'Key' defines the partition key and sort key of the item to be updated
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'eventId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expression
+    UpdateExpression: "SET name= :name, mainPhoto = :mainPhoto, description = :description",
+    ExpressionAttributeValues: {
+      ":name": data.name ? data.name : null,
+      ":mainPhoto": data.mainPhoto ? data.mainPhoto : null,
+      ":description": data.description ? data.description : null
+    },
+    ReturnValues: "ALL_NEW"
+  };
+
+  try {
+    const result = await dynamoDbLib.call("update", params);
+    callback(null, success({ status: true }));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
